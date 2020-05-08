d081ebedeb58798a6738d67b97bf344dd73cd3ed
EloLuigi <luis.freire@live.com.pt>
2020-05-08 14:52:36 +0200

    ll

---
+++ app/src/main/java/com/example/androidapp/APIAccess/RequestManager.java
@@ -0,0 +1,5 @@
+package com.example.androidapp.APIAccess;
+
+public class RequestManager {
+
+}
--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
+
+
 }
